5ab84d1ea63323e00c2dd18c78c56a89bda032cb
AaliyahSalia <101353473+AaliyahSalia@users.noreply.github.com>
2022-08-31 15:47:54 -0700

    Create EmailSlicer.py

---
+++ EmailSlicer.py
@@ -0,0 +1,5 @@
+email = input("Please enter your email ID: ").strip()
+username = email[:email.index("@")]
+domain_name = email[email.index("@")+1:]
+format_ = (f"Your username is: ' {username}' and your domain is: '{domain_name}'")
+print(format_)
